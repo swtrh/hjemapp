445f41aabe4bb5d27b112954582121301e3aff8e
Hallgeir Holien <halh@statoil.com>
2018-03-12 11:49:27 +0100

    Enkel navigering

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, View } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { StackNavigator, } from 'react-navigation';
 
 class HomeScreen extends Component {
@@ -12,7 +12,53 @@ class HomeScreen extends Component {
             <View>
                 <Button
                     title="Konfigurer"
+                    onPress={() =>
+                        navigate('Config')
+                    }
                 />
+                <Button
+                    title="Alle alarmer"
+                    onPress={() =>
+                        navigate('MonitorAll')
+                    }
+                />
+            </View>
+        );
+    }
+}
+
+class ConfigScreen extends Component {
+    static navigationOptions = {
+        title: 'Konfigurer HjemApp',
+    };
+    render() {
+        return (
+            <View>
+                <Text>Konfig her!</Text>
+            </View>
+        );
+    }
+}
+
+class MonitorOne extends Component {
+    render() {
+        return (
+            <Text>{this.props.name}</Text>
+        );
+    };
+}
+
+class MonitorAllScreen extends Component {
+    static navigationOptions = {
+        title: 'Alle alarmer',
+    };
+    render() {
+        return (
+            <View>
+                <MonitorOne name='Garasjedør' />
+                <MonitorOne name='Temperatur inne Reppe' />
+                <MonitorOne name='Vanntrykk Smibakken' />
+                <MonitorOne name='Temperatur Smibakken' />
             </View>
         );
     }
@@ -20,6 +66,8 @@ class HomeScreen extends Component {
 
 const App = StackNavigator({
     Home: { screen: HomeScreen },
+    Config: {screen: ConfigScreen},
+    MonitorAll:  {screen: MonitorAllScreen},
 });
 
 export default App;
