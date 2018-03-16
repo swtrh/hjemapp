a7b264bcde891aba470386ff52ff7bbc9f202e12
Hallgeir Holien <halh@statoil.com>
2018-03-16 14:16:03 +0100

    Separate mqtt from ui code

--- App.js
+++ App.js
@@ -1,71 +1,10 @@
 import React, { Component } from 'react';
 import { Button, View, Text, FlatList, TextInput, } from 'react-native';
 import { StackNavigator, } from 'react-navigation';
-import { Client, Message } from 'react-native-paho-mqtt';
-import { secrets } from './secrets';
-
+import { Message } from 'react-native-paho-mqtt';
+import {client, conn, messages} from "./events";
 
 const testTopic = "sb/e1/kjk/x";
-const testSubscribeTopic = "sb/e1/kjk/#";
-const testHost = "ws://broker.shiftr.io:80/ws";
-
-let messages = [];
-let connected = false;
-
-//Set up an in-memory alternative to global localStorage
-const myStorage = {
-    setItem: (key, item) => {
-        myStorage[key] = item;
-    },
-    getItem: (key) => myStorage[key],
-    removeItem: (key) => {
-        delete myStorage[key];
-    },
-};
-
-// Create a mqtt client instance
-const client = new Client({ uri: testHost, clientId: 'HjemApp', storage: myStorage });
-
-// set event handlers
-client.on('connectionLost', (responseObject) => {
-    if (responseObject.errorCode !== 0) {
-        connected = false;
-        messages.push("Lost conn");
-        console.log(responseObject.errorMessage);
-    }
-});
-
-client.on('messageReceived', (message) => {
-    console.log(message.payloadString);
-    messages.push(message.payloadString);
-});
-
-function conn() {
-    if (!connected) {
-        client.connect({ userName: secrets.username, password: secrets.password})
-            .then(() => {
-                // Once a connection has been made, make a subscription and send a message.
-                console.log('onConnect');
-                connected = true;
-                messages.push("Connected");
-                return client.subscribe(testSubscribeTopic);
-            })
-            .then(() => {
-                const message = new Message('Hello');
-                message.destinationName = testTopic;
-                client.send(message);
-            })
-            .catch((responseObject) => {
-                if (responseObject.errorCode !== 0) {
-                    console.log('onConnectionLost:' + responseObject.errorMessage);
-                }
-            })
-        ;
-    }
-}
-
-//Initial connection to message queue
-conn();
 
 class HomeScreen extends Component {
     static navigationOptions = {
@@ -99,6 +38,12 @@ class HomeScreen extends Component {
                         navigate('Messages')
                     }
                 />
+                <Button
+                    title="En måling"
+                    onPress={() =>
+                        navigate('MonitorOne', {tag: 'S_T1'})
+                    }
+                />
             </View>
         );
     }
@@ -117,18 +62,25 @@ class ConfigScreen extends Component {
     }
 }
 
-class MonitorOne extends Component {
+class MonitorOneScreen extends Component {
+    static navigationOptions = {
+        title: 'En måling',
+    };
+
+
     render() {
         return (
-            <Text>{this.props.name}</Text>
+            <View>
+                <Text>Tag: {this.props.navigation.state.params.tag} </Text>
+            </View>
         );
     };
 }
 
 const monitoring = [
-        {key: 'R_G1', name: 'Garasjedør'},
+        {key: 'R_G1', name: 'Garasjedør', topic: 'rp/h2/door', type: 'openclosed'},
         {key: 'R_T1', name: 'Temperatur inne Reppe'},
-        {key: 'S_T1', name: 'Temperatur inne Smibakken', topic: 'sb/e1/kjk/t', high: 30.0, low: 5.0},
+        {key: 'S_T1', name: 'Temperatur inne Smibakken', topic: 'sb/e1/kjk/t', type:'contin', high: 30.0, low: 5.0},
         {key: 'S_P1', name: 'Vanntrykk Smibakken'},
     ];
 
@@ -204,9 +156,7 @@ class BroadcastScreen extends Component {
     }
 
     componentDidMount() {
-        if (!connected) {
-            conn();
-        }
+        conn();
     }
 }
 
@@ -216,6 +166,7 @@ const App = StackNavigator({
     MonitorAll:  {screen: MonitorAllScreen},
     Messages: {screen: MessageScreen},
     Broadcast: {screen: BroadcastScreen},
+    MonitorOne:  {screen: MonitorOneScreen}, //( {tag: 'S_T1'} )
 });
 
 
---
+++ events.js
@@ -0,0 +1,65 @@
+import {Client, Message} from "react-native-paho-mqtt";
+import {secrets} from "./secrets";
+
+const testSubscribeTopic = "sb/e1/kjk/#";
+const testHost = "ws://broker.shiftr.io:80/ws";
+
+let messages = [];
+let connected = false;
+
+//Set up an in-memory alternative to global localStorage
+const myStorage = {
+    setItem: (key, item) => {
+        myStorage[key] = item;
+    },
+    getItem: (key) => myStorage[key],
+    removeItem: (key) => {
+        delete myStorage[key];
+    },
+};
+
+// Create a mqtt client instance
+const client = new Client({ uri: testHost, clientId: 'HjemApp', storage: myStorage });
+
+// set event handlers
+client.on('connectionLost', (responseObject) => {
+    if (responseObject.errorCode !== 0) {
+        connected = false;
+        messages.push("Lost conn");
+        console.log(responseObject.errorMessage);
+    }
+});
+
+client.on('messageReceived', (message) => {
+    console.log(message.payloadString);
+    messages.push(message.payloadString);
+});
+
+function conn() {
+    if (!connected) {
+        client.connect({ userName: secrets.username, password: secrets.password})
+            .then(() => {
+                // Once a connection has been made, make a subscription and send a message.
+                console.log('onConnect');
+                connected = true;
+                messages.push("Connected");
+                return client.subscribe(testSubscribeTopic);
+            })
+            .then(() => {
+                const message = new Message('Hello');
+                message.destinationName = testTopic;
+                client.send(message);
+            })
+            .catch((responseObject) => {
+                if (responseObject.errorCode !== 0) {
+                    console.log('onConnectionLost:' + responseObject.errorMessage);
+                }
+            })
+        ;
+    }
+}
+
+//Initial connection to message queue
+conn();
+
+export {conn, client, messages};
